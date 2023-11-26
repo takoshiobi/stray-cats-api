ef31aa5fa6cad3b532c625d17158ce322cbae031
Yulia Scherbakova <yscherbakova@dpd.ru>
2023-11-26 20:31:43 +0300

    init

---
+++ src/main/java/ru/sds/straycats/StrayCatsApplication.java
@@ -0,0 +1,13 @@
+package ru.sds.straycats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrayCatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrayCatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/sds/straycats/StrayCatsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sds.straycats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrayCatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
